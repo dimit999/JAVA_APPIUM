ab95e9d6b46ac34c7d8f98d4f81a70dfe2bfca54
dzmitryananyeu <DimITrY9D>
2025-05-10 15:04:38 +0400

    wallet creation e2e test

--- src/main/java/screens/android/AndroidScreenFactory.java
+++ src/main/java/screens/android/AndroidScreenFactory.java
@@ -1,18 +1,44 @@
 package screens.android;
 
-import io.appium.java_client.AppiumDriver;
-import screens.base.FirstScreen;
-import screens.base.ScreenFactory;
+import screens.android.introScreens.*;
+import screens.base.*;
+import screens.base.introScreens.*;
 
 public class AndroidScreenFactory implements ScreenFactory {
-    private AppiumDriver driver;
 
-    public AndroidScreenFactory(AppiumDriver driver) {
-        this.driver = driver;
+    @Override
+    public WalletEntryScreen walletEntryScreen() {
+        return new WalletEntryScreenAndroid();
     }
 
     @Override
-    public FirstScreen firstScreen() {
-        return new FirstScreenAndroid(this.driver);
+    public CreatePasscodeScreen createPasscodeScreen() {
+        return new CreatePasscodeScreenAndroid();
     }
+
+    @Override
+    public ConfirmPasscodeScreen confirmPasscodeScreen() {
+        return new ConfirmPasscodeScreenAndroid();
+    }
+
+    @Override
+    public EntryNotificationScreen entryNotificationScreen() {
+        return new EntryNotificationScreenAndroid();
+    }
+
+    @Override
+    public SuccessWalletReadyScreen successWalletReadyScreen() {
+        return new SuccessWalletReadyScreenAndroid();
+    }
+
+    @Override
+    public WhatIsNewScreen whatIsNewScreen() {
+        return new WhatIsNewScreenAndroid();
+    }
+
+    @Override
+    public MainScreen mainScreen() {
+        return new MainScreenAndroid();
+    }
+
 }
